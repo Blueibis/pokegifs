f2fe3a319d9ec293bd57ca15ecf2fe37c0b110c3
Blueibis <jennie.li.2011@gmail.com>
2017-06-28 18:22:12 -0400

    implement gif

--- app/controllers/pokemon_controller.rb
+++ app/controllers/pokemon_controller.rb
@@ -1,17 +1,18 @@
 class PokemonController < ApplicationController
 
-  def index
-    header = {
-      api_key: ENV["GIPHY_MYAPP_KEY"]
-     }
-    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/pikachu/", followlocation: true, headers: header)
+  def show
+
+    res = Typhoeus.get("http://pokeapi.co/api/v2/pokemon/#{params[:id]}/", followlocation: true)
     body = JSON.parse(res.body)
+    res2 = Typhoeus.get("https://api.giphy.com/v1/gifs/search?api_key=#{ENV["GIPHY_MYAPP_KEY"]}&q=#{body["name"]}&rating=g", followlocation: true)
+    body2 = JSON.parse(res2.body)
      # should be "pikachu"
     render json:
     {
       id: body["id"],
       name: body["name"],
-      types: body["types"].first["type"]["name"]
+      types: body["types"].first["type"]["name"],
+      gif: body2["data"].sample["embed_url"]
     }
   end
 
